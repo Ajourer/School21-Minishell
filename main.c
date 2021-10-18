5caa337fa27d1061bda4e47cd477e01522d0a7f7
Regine Alverta <ralverta@mi-h5.msk.21-school.ru>
2021-10-18 21:57:29 +0300

    Memory mistakes!

---
+++ main.c
@@ -0,0 +1,90 @@
+# include "parser.h"
+
+static char	*clean_quotes(char **str, int *i, int j)
+{
+	char	*res;
+	char	*res2;
+	char	*res3;
+	char	*tmp;
+
+	res = ft_substr(*str, 0, j);
+	res2 = ft_substr(*str, j + 1, *i - j - 1);
+	res3 = ft_strdup(*str + *i + 1);
+	tmp = res;
+	res = ft_strjoin(res, res2);
+	free(tmp);
+	free(res2);
+	tmp = res;
+	res = ft_strjoin(res, res3);
+	free(tmp);
+	free(res3);
+	return (res);
+}
+
+char	*single_quotes(char **str, int *i)
+{
+	int		j;
+	char	*res;
+
+	j = *i;
+	while ((*str)[++(*i)])
+		if ((*str)[*i] == '\'')
+			break ;
+	res = clean_quotes(str, i, j);
+	free(*str);
+	printf("i=%d single res = %s\n", *i,res);
+	*i -= 1;
+	return (res);
+}
+
+char	*double_quotes(char **str, int *i, char **env)
+{
+	int		j;
+	char	*res;
+
+	j = *i;
+	while ((*str)[++(*i)])
+	{
+		if ((*str)[*i] == '$')
+			*str = dollar(str, i, env);
+		if ((*str)[*i] == '\"')
+			break ;
+	}
+	res = clean_quotes(str, i, j);
+	free(*str);
+	printf("i=%d double res = %s\n",*i, res);
+	*i -= 1;
+	return (res);
+}
+
+void	parser(char **str, char **env)
+{
+	int	i;
+												// МЫ НЕ СОЗДАЕМ ЗДЕСЬ tmp, ЧТОБЫ ОЧИЩАТЬ str, КОТОРЫЙ ПЕРЕЗАПИСЫВАЕТСЯ
+	i = -1;
+	while ((*str)[++i])
+	{
+		if ((*str)[i] == '\'')
+			*str = single_quotes(str, &i);
+		if ((*str)[i] == '\"')
+			*str = double_quotes(str, &i, env);
+		if ((*str)[i] == '$')
+			*str = dollar(str, &i, env);
+		//write(1, "\033[31m", strlen("\033[32m"));
+		//write(1, &(*str)[i], 1);
+		//write(1, "\033[0m", strlen("\033[0m"));
+	}
+}
+
+int	main(int argc, char **argv, char **env)
+{
+	//char *str = ft_strdup("\"\'\"$USER\"\'\"");
+	//char *str = ft_strdup("\"\'\" $USER\"\'\"");
+	char *str = ft_strdup("'\"'$USER'\"'");
+
+	printf("str = %s\n", str);
+	parser(&str, env);
+	printf("result = %s\n", str);
+	free(str);
+	//while (1);
+}
